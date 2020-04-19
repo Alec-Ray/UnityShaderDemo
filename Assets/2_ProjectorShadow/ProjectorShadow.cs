9d047accab41da2fd993bed8c7ef5912a1fc4396
WJGwjg <1056929926@qq.com>
2020-04-19 15:50:12 +0800

    ProjectorShadow

---
+++ Assets/2_ProjectorShadow/ProjectorShadow.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectorShadow : MonoBehaviour
+{
+    public float m_ProjectorSize = 10;
+    public int m_RenderTexSize = 1024;
+    public LayerMask m_LayerCaster;
+    public LayerMask m_LayerIgnoreReceiver;
+    public Transform m_FollowObj;
+
+    private RenderTexture m_RenderTexture;
+    private Projector m_Projector;
+    private Camera m_Camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //RT
+        m_RenderTexture = new RenderTexture(m_RenderTexSize, m_RenderTexSize, 0, RenderTextureFormat.R8);
+        m_RenderTexture.name = "ShodowRT";
+        m_RenderTexture.antiAliasing = 1;
+        m_RenderTexture.filterMode = FilterMode.Bilinear;
+        //传递给Projector材质的RenderTexture必须是clamp模式,clamp会在Projector边界处的阴影出现长条，可用一张Mask Texture解决
+        m_RenderTexture.wrapMode = TextureWrapMode.Clamp;
+
+        //投影器设置
+        m_Projector = gameObject.GetComponent<Projector>();
+        m_Projector.orthographic = true;
+        m_Projector.orthographicSize = m_ProjectorSize;
+        m_Projector.ignoreLayers = m_LayerIgnoreReceiver;
+        m_Projector.material.SetTexture("_ShadowTex", m_RenderTexture);
+
+        //摄像机设置
+        m_Camera = gameObject.AddComponent<Camera>();
+        m_Camera.clearFlags = CameraClearFlags.Color;
+        m_Camera.backgroundColor = Color.black;
+        m_Camera.orthographic = true;
+        m_Camera.orthographicSize = m_ProjectorSize;
+        m_Camera.depth = -100;
+        m_Camera.nearClipPlane = m_Projector.nearClipPlane;
+        m_Camera.farClipPlane = m_Projector.farClipPlane;
+        m_Camera.targetTexture = m_RenderTexture;
+
+        //设置Camera渲染使用的shader
+        Shader replaceShader = Shader.Find("ShaderDemo/ProjectorShadowCaster");
+        m_Camera.cullingMask = m_LayerCaster;
+        m_Camera.SetReplacementShader(replaceShader, "RenderType");
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = m_FollowObj.transform.position - transform.forward * 50.0f;
+    }
+}
---
+++ Assets/2_ProjectorShadow/ProjectorShadowReceive.shader
@@ -0,0 +1,63 @@
+﻿Shader "ShaderDemo/ProjectorShadowReceive"
+{
+    Properties
+    {
+		//Projector上面的摄像机生成的RT
+		_ShadowTex("Shadow Texture", 2D) = "gray"{}
+		//控制阴影的强弱，远处阴影减弱
+		_MaskTex ("Texture", 2D) = "white" {}
+		//阴影强度
+		_Intensity("Intensity", Range(0,1)) = 0.5
+    }
+    SubShader
+    {
+        Tags { "RenderType"="AlphaTest+1" }
+        LOD 100
+
+        Pass
+        {
+			ZWrite off
+			ColorMask RGB
+			Blend DstColor Zero
+			Offset -1, -1
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct v2f
+            {
+				float4 pos:POSITION;
+				float4 sproj : TEXCOORD0;
+            };
+
+			float4x4 unity_Projector;	//投影矩阵
+			sampler2D _ShadowTex;
+			sampler2D _MaskTex;
+			float4 _MaskTex_ST;
+			float _Intensity;
+
+			v2f vert (float4 vertex:POSITION)
+			{
+				v2f o;
+				o.pos = UnityObjectToClipPos(vertex);
+				o.sproj = mul(unity_Projector, vertex);
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				//取投影的结果
+				half4 shadowCol = tex2Dproj(_ShadowTex, UNITY_PROJ_COORD(i.sproj));
+				half maskCol = tex2Dproj(_MaskTex, UNITY_PROJ_COORD(i.sproj)).r;
+				half a = shadowCol.r * maskCol;
+				float c = 1.0 - _Intensity * a;
+				//float c = 1.0 - _Intensity * shadowCol.r;
+				return c;
+			}
+            ENDCG
+        }
+    }
+}
